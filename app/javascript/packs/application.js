455a4bf59480dd9263c1898a85780de7fee5c379
MuhammedAlaa <soupermado@gmail.com>
2021-07-31 20:53:45 +0200

    adding the ui for the main page(welcome page) with semantic ui

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,10 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import '@doabit/semantic-ui-sass'
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+$(document).on("turbolinks:load", () => $(".ui.dropdown").dropdown());
