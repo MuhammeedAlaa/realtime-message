c17c32a3fc4d141e763dd22d8d2f6903295f1c9d
MuhammedAlaa <soupermado@gmail.com>
2021-08-01 04:11:18 +0200

    adding the flash ui messages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,4 +13,7 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-$(document).on("turbolinks:load", () => $(".ui.dropdown").dropdown());
+$(document).on("turbolinks:load", () => {
+    $(".ui.dropdown").dropdown();
+    $('.message .close').on('click',function () {$(this).closest('.message').transition('fade')});
+});
